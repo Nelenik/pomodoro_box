5575451c4b9a868e0abb3dd51240b6145891ae60
NeLeNik <lnek10.1990@gmail.com>
2024-03-21 01:37:29 +0200

    installed dependencies, setted environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), svgr()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src/"),
+      assets: `${path.resolve(__dirname, "./src/assets/")}`,
+      utils: `${path.resolve(__dirname, "./src/utils/")}`,
+      hooks: `${path.resolve(__dirname, "./src/hooks/")}`,
+      pages: `${path.resolve(__dirname, "./src/pages/")}`,
+      ui: `${path.resolve(__dirname, "./src/ui/")}`,
+      api: `${path.resolve(__dirname, "./src/api/")}`,
+    },
+  },
+});
